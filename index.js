d7e86a599b24295d01762c31f38e7b5c0a1d4d8c
Andy Lau <andyhplau@gmail.com>
2024-03-09 17:11:06 -0800

    fixed queries to prevent sql injection

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const session = require("express-session");
 const bcrypt = require("bcrypt");
 const cors = require("cors");
 const fs = require("fs");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 var urlencodedParser = bodyParser.json({
-  extended: false
-})
-
+  extended: false,
+});
 
 const databasePool = require("./databaseConnection");
 // const { printMySQLVersion } = include("database/db_utils");
@@ -37,24 +36,23 @@ app.use(
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 
-
 function reqLogin(req, res, next) {
   if (req.session.loggedIn) {
     next();
-} else {
-    res.redirect('/login');
+  } else {
+    res.redirect("/login");
     return;
-}
+  }
 }
 
 //#region PUBLIC PAGES
 //todo: change all route routes
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   let doc;
   if (!req.session.loggedIn) {
-      doc = fs.readFileSync('./frontend/login.html', "utf8");
+    doc = fs.readFileSync("./frontend/login.html", "utf8");
   } else {
-      doc = fs.readFileSync('./frontend/dashboard.html', "utf8");
+    doc = fs.readFileSync("./frontend/dashboard.html", "utf8");
   }
   res.send(doc);
 });
@@ -85,101 +83,117 @@ app.get("/community", function (req, res) {
 
 //todo: create/ delete/ edit account
 
-app.post('/api/login', urlencodedParser, function (req, res) {
+app.post("/api/login", urlencodedParser, function (req, res) {
   res.setHeader("Content-Type", "application/json");
   const password = req.body.password;
   const user = req.body.username ? req.body.username : req.body.email;
 
   //TODO: Make authentication system
   authenticate(user, password, (result) => {
-      if (result.status == 200) {
+    if (result.status == 200) {
+      req.session.loggedIn = true;
+      req.session.user_id = result.user.user_id;
+      req.session.username = result.user.username;
+      req.session.first_name = result.user.first_name;
+      req.session.last_name = result.user.last_name;
+      req.session.birth_date = result.user.birth_date;
+      req.session.gender = result.user.gender;
+      req.session.location = result.user.location;
+      req.session.quote = result.user.quote;
 
-          req.session.loggedIn = true;
-          req.session.user_id = result.user.user_id;
-          req.session.username = result.user.username;
-          req.session.first_name = result.user.first_name;
-          req.session.last_name = result.user.last_name;
-          req.session.birth_date = result.user.birth_date;
-          req.session.gender = result.user.gender;
-          req.session.location = result.user.location;
-          req.session.quote = result.user.quote;
-
-          req.session.save(function (err) {
-              //console.log(err);
-          });
-          res.status(200).send({
-              "result": "Successfully logged in."
-          })
-      } else {
-          res.status(400).send({
-              "result": "Failed to log in."
-          })
-      }
-  })
+      req.session.save(function (err) {
+        //console.log(err);
+      });
+      res.status(200).send({
+        result: "Successfully logged in.",
+      });
+    } else {
+      res.status(400).send({
+        result: "Failed to log in.",
+      });
+    }
+  });
 });
 
 app.get("/api/logout", function (req, res) {
   if (req.session) {
-      req.session.destroy(function (error) {
-          if (error) {
-              res.status(400).send({
-                  "result": "Failed",
-                  "msg": "Could not log out."
-              })
-          } else {
-              res.status(200).send({
-                  "result": "Succeeded",
-                  "msg": "Successfully logged out."
-              })
-          }
-      });
+    req.session.destroy(function (error) {
+      if (error) {
+        res.status(400).send({
+          result: "Failed",
+          msg: "Could not log out.",
+        });
+      } else {
+        res.status(200).send({
+          result: "Succeeded",
+          msg: "Successfully logged out.",
+        });
+      }
+    });
   }
 });
 
-app.get('/api/getUserInfo', urlencodedParser, function (req, res) {
-  if ((req.query.uid == undefined && req.query.username == undefined) || (req.query.uid == null && req.query.username == null)) {
-      if (req.session.loggedIn) {
-          //console.log(req.session)
-          res.send({
-              "loggedIn": true,
-              "userID": req.session.user_id,
-              "username": req.session.username,
-              "firstName": req.session.first_name,
-              "lastName": req.session.first_name,
-              "birthDate": req.session.birth_date,
-              "gender": req.session.gender,
-              "location": req.session.location,
-              "quote": req.session.quote,
-          });
-      } else {
-          res.send({
-              "loggedIn": false
-          });
-      }
-
-  } else {
-      let checkIfExists = `SELECT * FROM freedb_qds_team17.user WHERE username = '${req.query.username}' OR user_id = ${parseInt(req.query.uid ? req.query.uid : -1)}`;
-      databasePool.query(checkIfExists, (err, result) => {
-          if (result != null && result.length > 0) {
-              res.status(200).send({
-                  'result': 'Success',
-                  'msg': 'Sucessfully found user.',
-                  'userID': result[0].user_id,
-                  'username': result[0].username,
-                  "firstName": result[0].first_name,
-                  "lastName": result[0].first_name,
-                  "birthDate": result[0].birth_date,
-                  "gender": result[0].gender,
-                  "location": result[0].location,
-                  "quote": result[0].quote,
-                });
-          } else {
-              res.status(400).send({
-                  'result': 'Failed',
-                  'msg': 'User not found.'
-              })
-          }
+app.get("/api/getUserInfo", urlencodedParser, function (req, res) {
+  if (
+    (req.query.uid == undefined && req.query.username == undefined) ||
+    (req.query.uid == null && req.query.username == null)
+  ) {
+    if (req.session.loggedIn) {
+      //console.log(req.session)
+      res.send({
+        loggedIn: true,
+        userID: req.session.user_id,
+        username: req.session.username,
+        firstName: req.session.first_name,
+        lastName: req.session.first_name,
+        birthDate: req.session.birth_date,
+        gender: req.session.gender,
+        location: req.session.location,
+        quote: req.session.quote,
       });
+    } else {
+      res.send({
+        loggedIn: false,
+      });
+    }
+  } else {
+    let checkIfExists = `
+      SELECT
+        user_id,
+        username,
+        first_name,
+        last_name,
+        birth_date,
+        gender,
+        location,
+        quote
+      FROM user
+      WHERE username = :user OR user_id = :userId;`;
+    let params = {
+      username: req.query.username,
+      userId: parseInt(req.query.uid ? req.query.uid : -1),
+    };
+    databasePool.query(checkIfExists, params, (err, result) => {
+      if (result != null && result.length > 0) {
+        res.status(200).send({
+          result: "Success",
+          msg: "Sucessfully found user.",
+          userID: result[0].user_id,
+          username: result[0].username,
+          firstName: result[0].first_name,
+          lastName: result[0].first_name,
+          birthDate: result[0].birth_date,
+          gender: result[0].gender,
+          location: result[0].location,
+          quote: result[0].quote,
+        });
+      } else {
+        res.status(400).send({
+          result: "Failed",
+          msg: "User not found.",
+        });
+      }
+    });
   }
 });
 
