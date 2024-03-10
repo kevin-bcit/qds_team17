2800828883ea4304bfd72873e1daca94e47578e7
kevin ng <ong8@my.bcit.ca>
2024-03-10 04:41:51 -0700

    (kevin) partially updated community page

--- frontend/scripts/community-script.js
+++ frontend/scripts/community-script.js
@@ -1,19 +1,23 @@
 const apiUrl = 'http://localhost:3000';
 const endpointChallenge = '/api/getChallengeInfo';
 const endpointProgress = '/api/getProgress'
+const endpointUser = '/api/getUserInfo'
 
 const main = $('#community-main')
 const test = $('#wrapper')
-const challenge_ids = [1,2]
+const challenge_ids = [1,2, 4, 5, 6]
 const progress_ids = [1,2, 3, 8, 10, 11, 12, 13]
+const support = ["Almost halfway in collecting today’s apple. Wish me luck everyone!",
+    "Argh! Almost done all of deep breathing exercises for today...",
+    "Lets go!!! Just collected another apple today. 6th pie incoming!"]
 
 
 ;(async ()=> {
     const challengeData = []
 
     
-    challenge_ids.forEach(async (challenge_id) => {
-        const res = await fetch(`${apiUrl}${endpointChallenge}?challengeId=${challenge_ids[0]}`)
+    challenge_ids.forEach(async (cid) => {
+        const res = await fetch(`${apiUrl}${endpointChallenge}?challengeId=${cid}`)
         const data = await res.json()
         
         challengeData.push(data)
@@ -26,14 +30,20 @@ const progress_ids = [1,2, 3, 8, 10, 11, 12, 13]
         const res = await fetch(`${apiUrl}${endpointProgress}?progress_id=${progress_id}`)
         const pdata = await res.json()
         picNum = Math.floor(Math.random() * 7) + 1
-        challenge = 0
+        challenge = Math.floor(Math.random() * challengeData.length)
         console.log(challenge)
+        console.log(pdata)
+
+        // const ures = await fetch(`${apiUrl}${endpointUser}?uid=${pdata.user_id}`)
+        // const udata = await ures.json()
+        // console.log(udata)
+
 
         sectionBlock = `<section class="community-section" class="tag">
         <article>
             <figure><img src="./images/cat${picNum}.jpg"></img><figcaption>lifetime pies</figcaption></figure>
-            <span class="community-title">${challengeData[challenge].title}</span><span class="community-time">2 days 5 hours</span>
-            <p>${challengeData[challenge].description}</p>
+            <span class="community-title">${"NAME?"}</span><span class="community-time">2 days 5 hours</span>
+            <p>${support[Math.floor(Math.random() * support.length)]}</p>
             
         </article>
         <article class="community-progress">
@@ -60,9 +70,6 @@ const progress_ids = [1,2, 3, 8, 10, 11, 12, 13]
     
 
     })  
-    // const res = await fetch(`${apiUrl}${endpointProgress}?progress_id=${progress_id}`)
-    // const data = await res.json()
-    // console.log(data)
-    // console.log(data.msg);
+    
 
 })()
